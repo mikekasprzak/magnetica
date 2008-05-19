34ca2405be2eae4366460af14d0d125210d2b794
mike <mike@748e16ba-0062-cd48-8f81-643e656c0272>
2008-05-19 19:12:13 +0000

    Added support for drawing the 4 basic geometry types.  Also included the test map.
    git-svn-id: svn://10.0.50.55/magnetica/trunk@12 748e16ba-0062-cd48-8f81-643e656c0272

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 
 #include <AllegroGraphics/Graphics.h>
 #include <AllegroGraphics/GraphicsDraw.h>
+#include <AllegroGraphics/GraphicsDraw3D.h>
 
 #include <PolyMap/PolyMap.h>
 #include <AdvancedGeometry/PointVsSphere2D.h>
@@ -12,12 +13,87 @@
 #include <AdvancedGeometry/PointVsEdgedPolygon2D.h>
 
 
-using namespace std;
+//using namespace std;
+
+class cGame {
+public:
+	cPolyMap Map;
+	
+public:
+	inline cGame() :
+		Map( "TestMap.txt" )
+	{
+	}
+	
+	inline void Step() {
+		
+	}
+	
+	
+	inline void Draw() {
+		// Draw Elements //
+		for ( size_t idx = 0; idx < Map.Element.size(); idx++ ) {
+			// What kind of element is it? //
+			if ( Map.Element[idx].Type == PME_POLY ) {
+				// What type of Polygon is it? //
+				switch (Map.Element[idx].Type) {
+					default: 
+					case 1: {
+						gfxDrawClosedPolygonWithNormals( &Map.Element[idx].Vertex[0], Map.Element[idx].Vertex.size() );
+									
+						break;
+					}
+				};
+			}
+			
+			else if ( Map.Element[idx].Type == PME_SPHERE ) {
+				// What type of Sphere is it? //
+				switch (Map.Element[idx].Type) {
+					default: 
+					case 1: {
+						gfxDrawCircle( Map.Element[idx].Center, Map.Element[idx].Data[0].f);
+									
+						break;
+					}
+				};
+			}
+
+			else if ( Map.Element[idx].Type == PME_NODE ) {
+				// What type of Node is it? //
+				switch (Map.Element[idx].Type) {
+					default: 
+					case 1: {
+						gfxDrawRadiusBox( Map.Element[idx].Center, 3 );
+									
+						break;
+					}
+				};
+			}
+
+			else if ( Map.Element[idx].Type == PME_RECT ) {
+				// What type of Node is it? //
+				switch (Map.Element[idx].Type) {
+					default: 
+					case 1: {
+						gfxDrawRect( Map.Element[idx].Vertex[0], Map.Element[idx].Vertex[1] );
+									
+						break;
+					}
+				};
+			}
+		}
+		
+		
+	}
+};
+
 
 
 
 int main( int argc, char* argv[] ) {
 	gfxInit( 480, 320, false, 2 );
+	
+	cGame Game;
 
 
 	while( !gfxShutdown() ) {
@@ -25,6 +101,9 @@ int main( int argc, char* argv[] ) {
 		
 		// Note the cursor position //
 		Vector2D Mouse(mouse_x / ScreenScalar, mouse_y / ScreenScalar);
+		
+		
+		Game.Draw();
 		
 	
 	
